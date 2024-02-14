96d225026053c48a85e9e0604556fc6ec8f9c4b7
RohiniAgawane96 <rohiniagawane2426@gmail.com>
2024-02-14 10:28:33 +0530

    created ttobar with add icon navigating to addEmployee component

--- EmployeeManagement/src/app/app-routing.module.ts
+++ EmployeeManagement/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { ViewDetailsComponent } from './view-details/view-details.component';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {path:'',component:AppComponent},
+  {path:'add',component:AddEmployeeComponent},
+  {path:'viewDetails', component:ViewDetailsComponent},
+  {path:'update', component:UpdateEmployeeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
