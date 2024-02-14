dd0e1458e6436f1799f7845123c954ebc7d8758c
RohiniAgawane96 <rohiniagawane2426@gmail.com>
2024-02-14 13:02:24 +0530

    created service and registartion form validation

--- EmployeeManagement/src/app/add-employee/add-employee.component.ts
+++ EmployeeManagement/src/app/add-employee/add-employee.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { EmailValidator, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-employee',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-employee.component.css']
 })
 export class AddEmployeeComponent {
+
+  constructor(private fb: FormBuilder) { }
+
+  RegistrationForm= this.fb.group({
+    name:[''],
+    email:['',EmailValidator],
+    contact: ['',Validators],
+    gender:['']
+  })
+  
+
+
+  onSubmit(){}
 
 }
