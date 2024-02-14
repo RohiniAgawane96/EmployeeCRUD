074b128524a16f2f3b28118ffaaa5beeda8ef353
RohiniAgawane96 <rohiniagawane2426@gmail.com>
2024-02-14 09:13:27 +0530

    created neccesary components

--- EmployeeManagement/src/app/app.module.ts
+++ EmployeeManagement/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { ViewDetailsComponent } from './view-details/view-details.component';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddEmployeeComponent,
+    ViewDetailsComponent,
+    UpdateEmployeeComponent
   ],
   imports: [
     BrowserModule,
